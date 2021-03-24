ee996fbca12f77d1f6df87b7210066c0532964c3
jasilven <jasilven@gmail.com>
2021-03-24 18:07:44 +0200

    conf

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TOOLS="alacritty ripgrep exa fd-find bat tealdeer starship"
+TOOLS="alacritty ripgrep exa fd-find bat tealdeer starship tokei drill"
 
 if [ -x ~/.cargo/bin/cargo ]; then
     for tool in $TOOLS; do

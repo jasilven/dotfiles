93aa9f94fdb09fe051973a6c7fcde4f762d4fb6a
jasilven <jasilven@gmail.com>
2021-06-15 08:51:17 +0300

    conf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,6 +13,8 @@ set -gx LC_ALL en_US.UTF-8
 set -gx FZF_DEFAULT_COMMAND fd --type f -H || git ls-tree -r --name-only HEAD || rg --files || ag -l -g \"\" || find .
 set -gx TERM xterm-256color
 set -gx RIPGREP_CONFIG_PATH $HOME/dotfiles/ripgreprc
+set -gx FZF_DEFAULT_OPTS --layout=reverse --color=bg+:#D9D9D9,bg:-1,border:#C8C8C8,spinner:#719899,hl:#f65c09,fg:#616161,header:#719872,info:#727100,pointer:#E12672,marker:#E17899,fg+:#000000,preview-bg:#D9D9D9,prompt:#0099BD,hl+:#f65c09
+set -gx BAT_THEME 'OneHalfLight'
 
 starship init fish | source
 
@@ -24,20 +26,20 @@ function cd
     end
 end
 
-function Light 
-    set -gx FZF_DEFAULT_OPTS --layout=reverse --color=bg+:#D9D9D9,bg:-1,border:#C8C8C8,spinner:#719899,hl:#f65c09,fg:#616161,header:#719872,info:#727100,pointer:#E12672,marker:#E17899,fg+:#000000,preview-bg:#D9D9D9,prompt:#0099BD,hl+:#f65c09
-    sed -i 's/colors: \*dark/colors: \*light/g' ~/dotfiles/config/alacritty/alacritty.yml
-    set -gx BAT_THEME 'OneHalfLight'
-end
-
-
-function Dark 
-    set -gx FZF_DEFAULT_OPTS --layout=reverse --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1
-    sed -i 's/colors: \*light/colors: \*dark/g' ~/dotfiles/config/alacritty/alacritty.yml
-    set -gx BAT_THEME 'OneHalfDark'
-end
-
-Light
+#function Light 
+#    set -gx FZF_DEFAULT_OPTS --layout=reverse --color=bg+:#D9D9D9,bg:-1,border:#C8C8C8,spinner:#719899,hl:#f65c09,fg:#616161,header:#719872,info:#727100,pointer:#E12672,marker:#E17899,fg+:#000000,preview-bg:#D9D9D9,prompt:#0099BD,hl+:#f65c09
+#    sed -i 's/colors: \*dark/colors: \*light/g' ~/dotfiles/config/alacritty/alacritty.yml
+#    set -gx BAT_THEME 'OneHalfLight'
+#end
+#
+#
+#function Dark 
+#    set -gx FZF_DEFAULT_OPTS --layout=reverse --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1
+#    sed -i 's/colors: \*light/colors: \*dark/g' ~/dotfiles/config/alacritty/alacritty.yml
+#    set -gx BAT_THEME 'OneHalfDark'
+#end
+#
+#Light
 
 function gl
     git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
